d8cc5d1c88101915352b2c6a009a078810ec65f7
Patricia-Berges <patry37.zgz@gmail.com>
2024-09-11 21:48:32 +0000

    Light function working!

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
-//create your first component
-const Home = () => {
+const TrafficLight = () => {
+	const poleStyle = {
+		width: "8px",
+		height: "100px",
+		background: "black",
+		margin: "auto"
+	}
+	const lightStyle = {
+        width: "50px",
+        height: "50px",
+        background: "black",
+        margin: "auto",
+        position: "relative"
+    }
+    const lightClasses = {red: "red light", yellow: "yellow light", green: "green light"}
+    
+    const [lights, setLights] = useState(lightClasses)
+
+    const lighted = (color) => {
+        if (color === "red"){
+            setLights({
+                red: "red light selected",
+                yellow: "yellow light",
+                green: "green light"
+            });
+        }
+        else if (color === "yellow"){
+            setLights({
+                red: "red light",
+                yellow: "yellow light selected",
+                green: "green light"
+            })
+        }
+        else if (color === "green"){
+            setLights({
+                red: "red light",
+                yellow: "yellow light",
+                green: "green light selected"
+            })
+        }
+    }
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div>
+			<div style={poleStyle}></div>
+			<div className="top" style={lightStyle}>
+                <div className={lights.red} onClick={() => lighted("red")}></div>
+            </div>
+            <div className="center" style={lightStyle}>
+                <div className={lights.yellow} onClick={() => lighted("yellow")}></div>
+            </div>
+            <div className="bottom" style={lightStyle}>
+                <div className={lights.green} onClick={() => lighted("green")}></div>
+            </div>
 		</div>
+		
 	);
 };
 
-export default Home;
+export default TrafficLight;
