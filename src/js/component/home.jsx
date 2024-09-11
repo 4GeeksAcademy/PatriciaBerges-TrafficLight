7cf5b0050e9e2aba70b34309efc26cdffe7620a4
Patricia-Berges <patry37.zgz@gmail.com>
2024-09-11 22:44:26 +0000

    Added bonus points!

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -47,22 +47,82 @@ const TrafficLight = () => {
         setLights({red: "red light", yellow: "yellow light", green: "green light"})
     }
 
+    const rotateLight = () => {
+        if (lights.red === "red light selected" ){
+            setLights({
+                red: "red light",
+                yellow: "yellow light",
+                green: "green light selected"
+            })
+        }
+        else if (lights.green === "green light selected"){
+            setLights({
+                red: "red light",
+                yellow: "yellow light selected",
+                green: "green light"
+            })
+        }
+        else if (lights.yellow === "yellow light selected"){
+            setLights({
+                red: "red light selected",
+                yellow: "yellow light",
+                green: "green light"
+            })
+        }
+    }
+
+    let [purple, setPurple] = useState(false)
+
+    const togglePurple = () => {
+        if (purple === false){setPurple(true)}
+        else if (purple === true){setPurple(false)}
+    }
+
+    const purpleLight = () => {
+        if (purple === true){
+            
+            return (
+                <div>
+                    <div style={lightStyle}>
+                        <div className={lights.green} onClick={() => lighted("green")}></div>
+                    </div>
+                    <div className="bottom" style={lightStyle}>
+                        <div className="purple light selected"></div>
+                    </div>
+                </div>
+            )    
+        }
+        else return (<div className="bottom" style={lightStyle}>
+            <div className={lights.green} onClick={() => lighted("green")}></div>
+        </div>)
+    }
+
 	return (
-		<div className="father">
+		<div >
 			<div style={poleStyle}></div>
 			<div className="top" style={lightStyle}>
                 <div className={lights.red} onClick={() => lighted("red")}></div>
             </div>
-            <div className="center" style={lightStyle}>
+            <div style={lightStyle}>
                 <div className={lights.yellow} onClick={() => lighted("yellow")}></div>
             </div>
-            <div className="bottom" style={lightStyle}>
-                <div className={lights.green} onClick={() => lighted("green")}></div>
+            {purpleLight()}
+            <div>
+                <button className="mt-5 button" onClick={lightsOff}>
+                    Lights off!
+                </button>
             </div>
-            <button className="mt-5 off" onClick={lightsOff}>
-                Lights off!
-            </button>
-		</div>
+            <div>
+                <button className="mt-2 button" onClick={rotateLight}>
+                    Light change!
+                </button>
+            </div>
+            <div>
+                <button className="mt-2 button" onClick={togglePurple}>
+                    Toggle Purple!
+                </button>
+            </div>
+        </div>
 		
 	);
 };
